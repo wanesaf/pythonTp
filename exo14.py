9faec8de6401c5fc57b6c14cb697ca9fd5683ffb
wanesaf <safwanesouhil.dendani@etu.usthb.dz>
2024-12-10 18:40:01 +0100

    c'est bon

---
+++ exo14.py
@@ -0,0 +1,12 @@
+string = input("Type in the word: ")
+for i in range (30) : 
+    if (i!=29):
+     print('*',end='')
+    else : 
+     print('*')
+
+
+print(string.center(30))
+
+for i in range (30) : 
+     print('*',end='')
