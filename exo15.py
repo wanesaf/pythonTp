a56028b903d1411dc581ab7403ce66356c37355b
wanesaf <safwanesouhil.dendani@etu.usthb.dz>
2024-12-10 18:20:34 +0100

    didint understand the concept of the exercice n14

---
+++ exo15.py
@@ -0,0 +1,14 @@
+string  = input("Please type in a string: ").lower()
+if (string.__contains__('a')):
+    print('a found')
+else: 
+    print('a not  found')
+if (string.__contains__('e')):
+    print('e found')
+else: 
+    print('e not found')
+if (string.__contains__('o')):
+    print('o found')
+else: 
+    print('o not found')
+    
